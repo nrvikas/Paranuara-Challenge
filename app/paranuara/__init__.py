fe5ac3ad707c37be49a359db424b2eae6abf7880
nrvikas@gmail.com <nrvikas@gmail.com>
2020-03-27 11:16:53 +1100

    added views to main file

--- app/paranuara/__init__.py
+++ app/paranuara/__init__.py
@@ -3,8 +3,13 @@
 This script initializes the 'paranuara' blueprint
 """
 
-from flask import Blueprint
-
+from flask import Blueprint, render_template
 paranuara_bp = Blueprint('paranuara', __name__)
 
-from app.paranuara.views import paranuara_index  # noqa
+
+@paranuara_bp.route('/')
+def paranuara_index():
+    """
+    Default landing page with API documentation
+    """
+    return render_template('index.html')
